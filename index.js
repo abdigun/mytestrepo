3649bc9172adbbb91f1af8e05a87e4f743d039a8
azahan <mhd.azahan90@gmail.com>
2021-08-24 14:54:00 +0800

    finished working with api

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("test");
+// we want to talk to an API
+// what is my endpoint ?
+
+let url = `https://restcountries.eu/rest/v2/name/Malaysia`;
+// what parameter are required?
+// what parameter are optional?
+// what is HTTP method ?
+
+function waitfordata(res) {
+	return res.json();
+}
+
+function handledata(data) {
+	let pop = data[0].population;
+	console.log(`the population of malaysia is ${pop}`);
+}
+
+fetch(url, { method: "GET" }).then(waitfordata).then(handledata);
